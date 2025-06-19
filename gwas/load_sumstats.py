d96ee53095e4572074564c7f17e5beee338c0cb9
Ofgeha-Gelana <ofgehagelana2019@gmail.com>
2025-06-19 12:40:06 +0300

    adding fully working getting lead varient

--- gwas/load_sumstats.py
+++ gwas/load_sumstats.py
@@ -5,4 +5,4 @@ input_file = sys.argv[1]
 output_file = sys.argv[2]
 
 sumstats = gl.Sumstats(input_file, fmt="plink2")
-sumstats.df.to_csv(output_file, sep="\t", index=False)
+sumstats.data.to_csv(output_file, sep="\t", index=False)
